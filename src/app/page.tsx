b2b654055664f60ecc8cb4a5ee11eb9eeb3cea1d
franco-barros <fb6776928@gmail.com>
2025-03-07 21:41:19 -0300

    Toggletheme || finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Hero from "../Components/hero";
 import AboutMe from "../Components/aboutme";
 import Projects from "../Components/projects";
 import Skills from "../Components/skills/Skills";
+import MouseEffect from "../Components/mouseeffect"; // Importa MouseEffect
 
 const HomePage: React.FC = () => {
   const [showGreeting, setShowGreeting] = useState<boolean>(true);
@@ -18,48 +19,50 @@ const HomePage: React.FC = () => {
   }, []);
 
   return (
-    <AnimatePresence>
-      {showGreeting && (
-        <motion.div
-          key="greetingOverlay"
-          initial={{ opacity: 1, y: 0 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: "100%" }}
-          transition={{ duration: 1 }}
+    <MouseEffect>
+      <AnimatePresence>
+        {showGreeting && (
+          <motion.div
+            key="greetingOverlay"
+            initial={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={{ duration: 1 }}
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              zIndex: 10000,
+              backgroundColor: "#f9f9f9",
+            }}
+          >
+            <Greeting />
+          </motion.div>
+        )}
+
+        <div
           style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            zIndex: 10000,
-            backgroundColor: "#f9f9f9",
+            opacity: showGreeting ? 0 : 1,
+            transition: "opacity 1s ease-in-out",
           }}
         >
-          <Greeting />
-        </motion.div>
-      )}
-
-      <div
-        style={{
-          opacity: showGreeting ? 0 : 1,
-          transition: "opacity 1s ease-in-out",
-        }}
-      >
-        <section id="home">
-          <Hero />
-        </section>
-        <section id="about">
-          <AboutMe />
-        </section>
-        <section id="skills">
-          <Skills />
-        </section>
-        <section id="projects">
-          <Projects />
-        </section>
-      </div>
-    </AnimatePresence>
+          <section id="home">
+            <Hero />
+          </section>
+          <section id="about">
+            <AboutMe />
+          </section>
+          <section id="skills">
+            <Skills />
+          </section>
+          <section id="projects">
+            <Projects />
+          </section>
+        </div>
+      </AnimatePresence>
+    </MouseEffect>
   );
 };
 
